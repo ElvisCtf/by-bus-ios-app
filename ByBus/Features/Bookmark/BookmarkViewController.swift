09b18b04ce36afeab73640b3c05ce7eb24fac37b
Elvis Cheng (ESD - Software Trainee, Digital Solutions) <elvistfcheng@esdlife.com>
2024-11-26 17:18:08 +0800

    feat: bottom tab bar with 3 tabs - route, bookmark and setting

---
+++ ByBus/Features/Bookmark/BookmarkViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BookmarkViewController.swift
+//  ByBus
+//
+//  Created by Elvis Cheng on 26/11/2024.
+//
+
+import UIKit
+
+class BookmarkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
