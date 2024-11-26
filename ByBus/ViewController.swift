c02cc36453e455f5ef3ab57452c608dcfc25a356
Elvis Cheng (ESD - Software Trainee, Digital Solutions) <elvistfcheng@esdlife.com>
2024-11-26 15:36:43 +0800

    Initial Commit

---
+++ ByBus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ByBus
+//
+//  Created by Elvis Cheng (ESD - Software Trainee, Digital Solutions) on 26/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
