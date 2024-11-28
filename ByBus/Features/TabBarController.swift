a409fee2bea5de33cf1e57d857bf00d9f9fda6b1
ElvisCtf <elvischengtf@gmail.com>
2024-11-28 16:30:19 +0800

    refactor: simplify code and enhance encapsulation in TabBarController

--- ByBus/Features/TabBarController.swift
+++ ByBus/Features/TabBarController.swift
@@ -7,38 +7,12 @@
 
 import UIKit
 
-class TabBarController: UITabBarController {
-    lazy var routeTab = RouteTab
-    lazy var bookmarkTab = BookmarkTab
-    lazy var settingTab = SettingTab
+final class TabBarController: UITabBarController {
+    private var routeTab    = UINavigationController(title: "Route", defaultImage: "bus", selectedImage: "bus.fill", viewController: RouteViewController())
+    private var bookmarkTab = UINavigationController(title: "Bookmark", defaultImage: "bookmark", selectedImage: "bookmark.fill", viewController: BookmarkViewController())
+    private var settingTab  = UINavigationController(title: "Setting", defaultImage: "gearshape", selectedImage: "gearshape.fill", viewController: SettingViewController())
     
     override func viewDidLoad() {
         self.viewControllers = [routeTab, bookmarkTab, settingTab]
-    }
-}
-
-
-// MARK: - UI
-extension TabBarController: UITabBarControllerDelegate {
-    var RouteTab: UINavigationController {
-        return buildTab(title: "Route", defaultImage: "bus", selectedImage: "bus.fill", viewController: RouteViewController())
-    }
-    
-    var BookmarkTab: UINavigationController {
-        return buildTab(title: "Bookmark", defaultImage: "bookmark", selectedImage: "bookmark.fill", viewController: BookmarkViewController())
-    }
-    
-    var SettingTab: UINavigationController {
-        return buildTab(title: "Setting", defaultImage: "gearshape", selectedImage: "gearshape.fill", viewController: SettingViewController())
-    }
-    
-    private func buildTab(title: String, defaultImage: String, selectedImage: String, viewController: UIViewController) -> UINavigationController {
-        let controller = UINavigationController(rootViewController: viewController)
-        let defaultImage = UIImage(systemName: defaultImage)
-        let selectedImage = UIImage(systemName: selectedImage)
-        let tabBarItems = (title: title, image: defaultImage, selectedImage: selectedImage)
-        let tabBarItem = UITabBarItem(title: tabBarItems.title, image: tabBarItems.image, selectedImage: tabBarItems.selectedImage)
-        controller.tabBarItem = tabBarItem
-        return controller
     }
 }
