297e63cc20473d202f1f9601ba85f248ff6d9d68
ElvisCtf <elvischengtf@gmail.com>
2025-01-24 17:43:00 +0800

    feat: Endpoints, APIService, NetworkManager and responseDTO

---
+++ ByBus/Services/APIService/APIService.swift
@@ -0,0 +1,14 @@
+//
+//  APIService.swift
+//  ByBus
+//
+//  Created by Elvis Cheng (ESD - Software Trainee, Digital Solutions) on 21/1/2025.
+//
+
+import Foundation
+
+final class APIService: APIServiceProtocol {
+    func getRouteList(completion: @escaping (Bool, RouteListResponseDto?, (any Error)?) -> Void) {
+        
+    }
+}
---
+++ ByBus/Services/APIService/Models/RouteListResponseDto.swift
@@ -0,0 +1,46 @@
+//
+//  RouteListResponseDto.swift
+//  ByBus
+//
+//  Created by Elvis Cheng (ESD - Software Trainee, Digital Solutions) on 20/1/2025.
+//
+
+import Foundation
+
+// MARK: - RouteListResponseDto
+struct RouteListResponseDto: Codable {
+    let type, version: String?
+    let generatedTimestamp: Date?
+    let data: [Datum]?
+
+    enum CodingKeys: String, CodingKey {
+        case type, version
+        case generatedTimestamp = "generated_timestamp"
+        case data
+    }
+}
+
+// MARK: - Datum
+struct Datum: Codable {
+    let company: Company?
+    let routeNo: String?
+    let origTc, origEn, origSc: String?
+    let destTc, destEn, destSc: String?
+    let dataTimestamp: Date?
+
+    enum CodingKeys: String, CodingKey {
+        case company = "co"
+        case routeNo = "route"
+        case origTc = "orig_tc"
+        case origEn = "orig_en"
+        case destTc = "dest_tc"
+        case destEn = "dest_en"
+        case origSc = "orig_sc"
+        case destSc = "dest_sc"
+        case dataTimestamp = "data_timestamp"
+    }
+}
+
+enum Company: String, Codable {
+    case ctb = "CTB"
+}
