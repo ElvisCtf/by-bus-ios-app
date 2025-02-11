a5b9753496b9aad39f331862dfb41c82cb2393af
Elvis Cheng (ESD - Software Trainee, Digital Solutions) <elvistfcheng@esdlife.com>
2025-02-11 15:40:27 +0800

    feat: get route data through APIService and NetworkManager

--- ByBus/Services/APIService/Endpoints.swift
+++ ByBus/Services/APIService/Endpoints.swift
@@ -5,6 +5,8 @@
 //  Created by Elvis Cheng (ESD - Software Trainee, Digital Solutions) on 20/1/2025.
 //
 
+import Foundation
+
 struct Endpoints {
-    static let routeList = "https://rt.data.gov.hk/v2/transport/citybus/route/ctb"
+    static let routeList = URL(string: "https://rt.data.gov.hk/v2/transport/citybus/route/ctb")!
 }
--- ByBus/Services/APIService/NetworkManager.swift
+++ ByBus/Services/APIService/NetworkManager.swift
@@ -6,13 +6,29 @@
 //
 
 import Foundation
+import RxSwift
 
 final class NetworkManager {
     static let shared = NetworkManager()
+    private let session: URLSession
     
-    private init() {}
+    private init() {
+        self.session = URLSession(configuration: .default)
+    }
     
-    func post() {
-        
+    func send<T: Decodable>(with request: URLRequest, as type: T.Type) -> Single<T> {
+        return Single<T>.create { observer in
+            let task = self.session.dataTask(with: request) { data, response, error in
+                if let error {
+                    observer(.failure(error))
+                } else if let data {
+                    observer(data.decode(as: T.self))
+                } else {
+                    observer(.failure(NSError(domain: "No data", code: -1, userInfo: nil)))
+                }
+            }
+            task.resume()
+            return Disposables.create { task.cancel() }
+        }
     }
 }
