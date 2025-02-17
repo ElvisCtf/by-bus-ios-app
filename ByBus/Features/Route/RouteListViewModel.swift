6c5b29ed27f9a52348a596c233853d0d12115c13
Elvis Cheng (ESD - Software Trainee, Digital Solutions) <elvistfcheng@esdlife.com>
2025-02-17 18:02:15 +0800

    feat: search route by route number

--- ByBus/Features/Route/RouteListViewModel.swift
+++ ByBus/Features/Route/RouteListViewModel.swift
@@ -10,7 +10,8 @@ import RxCocoa
 
 final class RouteListViewModel {
     let routeListObservable = BehaviorRelay<[Route]>.init(value: [])
-    
+    private var routeList = [Route]()
+    private var filteredRouteList = [Route]()
     private let apiService: APIService
     
     init(apiServie: APIService = APIService()) {
@@ -21,11 +22,27 @@ final class RouteListViewModel {
         apiService.getRouteList { [weak self] success, data, error in
             guard let self else { return }
             if success {
-                if let safeData = data, let routeList = safeData.data {
-                    self.routeListObservable.accept(routeList)
+                if let safeData = data, let fetchedRouteList = safeData.data {
+                    self.routeList = fetchedRouteList
+                    self.routeListObservable.accept(self.routeList)
                 }
             }
             
         }
+    }
+    
+    func filterRouteList(by searchingText: String, isSearchBarActive: Bool) {
+        if searchingText.isEmpty || !isSearchBarActive {
+            self.routeListObservable.accept(routeList)
+            return
+        }
+        
+        filteredRouteList = routeList.filter {
+            if let routeNo = $0.routeNo {
+                return routeNo.lowercased().contains(searchingText.lowercased())
+            }
+            return false
+        }
+        self.routeListObservable.accept(filteredRouteList)
     }
 }
