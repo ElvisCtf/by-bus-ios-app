9e7a5d9dd31d45396a6a46d370dfae3e4c283468
Elvis Cheng (ESD - Software Trainee, Digital Solutions) <elvistfcheng@esdlife.com>
2025-03-07 10:46:47 +0800

    feat: added UITableView to show bus stop name and estimated arrive time

---
+++ ByBus/Features/BusStop/BusStop.swift
@@ -0,0 +1,17 @@
+//
+//  BusStop.swift
+//  ByBus
+//
+//  Created by Elvis Cheng on 7/3/2025.
+//
+
+struct BusStop {
+    let index: Int
+    let name: String
+    var arriveTimeList: [String]
+    var isExpanded = false
+    
+    var arriveTime: String {
+        arriveTimeList.joined(separator: "\n\n")
+    }
+}
