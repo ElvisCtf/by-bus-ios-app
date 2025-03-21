a47408934606d79fc276f9d09653d1c98009dbd3
Elvis Cheng (ESD - Software Trainee, Digital Solutions) <elvistfcheng@esdlife.com>
2025-03-21 10:51:32 +0800

    refactor: RoutesView cell ui

--- ByBus/Features/Route/Subviews/RouteCellView.swift
+++ ByBus/Features/Route/Subviews/RouteCellView.swift
@@ -8,20 +8,22 @@
 import UIKit
 import SnapKit
 
-final class RouteCellView: UITableViewCell {
-    static let reuseID = "RouteCellView"
+class RouteCellView: UITableViewCell {
+    class var reuseID: String {
+        return "RouteCellView"
+    }
     
-    private let companyLbl = UILabel.plain(id: UI.company.id, lines: 1, weight: .regular, size: 13, color: .label)
-    private let routeNoLbl = UILabel.plain(id: UI.routeNo.id, lines: 1, alignment: .center, weight: .semibold, size: 17, color: .label)
-    private let originLbl  = UILabel.plain(id: UI.origin.id, lines: 1, weight: .regular, size: 17, color: .label)
-    private let destinLbl  = UILabel.plain(id: UI.destin.id, lines: 1, weight: .semibold, size: 17, color: .label)
-    private let fromLbl    = UILabel.plain(id: UI.from.id, lines: 1, weight: .regular, size: 13, color: .label)
-    private let toLbl      = UILabel.plain(id: UI.to.id, lines: 1, weight: .regular, size: 13, color: .label)
+    let rootView   = UIView.plain(cornerRadius: 8, bgColor: .systemBackground)
+    let companyLbl = UILabel.plain(id: UI.company.id, lines: 1, weight: .regular, size: 13, color: .label)
+    let routeNoLbl = UILabel.plain(id: UI.routeNo.id, lines: 1, alignment: .center, weight: .semibold, size: 17, color: .label)
+    let originLbl  = UILabel.plain(id: UI.origin.id, lines: 1, weight: .regular, size: 17, color: .label)
+    let destinLbl  = UILabel.plain(id: UI.destin.id, lines: 1, weight: .regular, size: 17, color: .label)
+    let fromLbl    = UILabel.plain(id: UI.from.id, lines: 1, weight: .light, size: 13, color: .label)
+    let toLbl      = UILabel.plain(id: UI.to.id, lines: 1, weight: .light, size: 13, color: .label)
     
-    private let lblVstack  = UIStackView.vertical(spacing: 8, padding: .zero)
-    private let lblHstack1 = UIStackView.horizontal(spacing: 4, padding: .zero, distribution: .fill)
-    private let lblHstack2 = UIStackView.horizontal(spacing: 4, padding: .zero, distribution: .fill)
-    private let separator  = UIView.plain(bgColor: .separator)
+    let lblVstack  = UIStackView.vertical(spacing: 8, padding: .zero)
+    let lblHstack1 = UIStackView.horizontal(spacing: 4, padding: .zero, distribution: .fill)
+    let lblHstack2 = UIStackView.horizontal(spacing: 4, padding: .zero, distribution: .fill)
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -29,7 +31,7 @@ final class RouteCellView: UITableViewCell {
         setLayout()
     }
     
-    private func setUI() {
+    func setUI() {
         selectionStyle = .none
         
         companyLbl.setContentHuggingPriority(.defaultLow + 2, for: .horizontal)
@@ -37,19 +39,24 @@ final class RouteCellView: UITableViewCell {
         toLbl.setContentHuggingPriority(.defaultLow + 1, for: .horizontal)
         fromLbl.setContentHuggingPriority(.defaultLow + 1, for: .horizontal)
         
-        lblHstack1.alignment = .bottom
-        lblHstack2.alignment = .bottom
+        rootView.border(color: .separator)
+        
+        backgroundColor = .clear
     }
     
-    private func setLayout() {
-        contentView.addSubview(companyLbl)
-        contentView.addSubview(routeNoLbl)
-        contentView.addSubview(lblVstack)
-        contentView.addSubview(separator)
+    func setLayout() {
+        contentView.addSubview(rootView)
+        rootView.addSubview(companyLbl)
+        rootView.addSubview(routeNoLbl)
+        rootView.addSubview(lblVstack)
         
         lblHstack1.addSubviews([fromLbl, originLbl])
         lblHstack2.addSubviews([toLbl, destinLbl])
         lblVstack.addSubviews([lblHstack1, lblHstack2])
+        
+        rootView.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(8)
+        }
         
         companyLbl.snp.makeConstraints {
             $0.left.equalToSuperview().inset(16)
@@ -67,11 +74,6 @@ final class RouteCellView: UITableViewCell {
             $0.right.equalToSuperview().inset(8)
             $0.top.bottom.equalToSuperview().inset(12)
         }
-        
-        separator.snp.makeConstraints {
-            $0.height.equalTo(1)
-            $0.left.right.bottom.equalToSuperview()
-        }
     }
     
     func setText(with route: Route) {
@@ -79,15 +81,6 @@ final class RouteCellView: UITableViewCell {
         routeNoLbl.text = route.routeNo
         destinLbl.text  = route.destTc
         originLbl.text  = route.origTc
-        fromLbl.text    = "由"
-        toLbl.text      = "往"
-    }
-    
-    func setText(with bookmark: BusStopBookmark) {
-        companyLbl.text = Company.ctb.zhName
-        routeNoLbl.text = bookmark.routeNo
-        destinLbl.text  = bookmark.origin.tc
-        originLbl.text  = bookmark.destination.tc
         fromLbl.text    = "由"
         toLbl.text      = "往"
     }
