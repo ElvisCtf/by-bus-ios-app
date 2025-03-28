f6a0d0a51ec2a7a6e92522d34324c4a652ce9856
ElvisCtf <elvischengtf@gmail.com>
2025-03-28 17:18:56 +0800

    refactor: adjust label width in RouteCellView and BookmarkCellView

--- ByBus/Features/Route/Subviews/RouteCellView.swift
+++ ByBus/Features/Route/Subviews/RouteCellView.swift
@@ -14,16 +14,17 @@ class RouteCellView: UITableViewCell {
     }
     
     let rootView   = UIView.plain(cornerRadius: 8, bgColor: .systemBackground)
-    let companyLbl = UILabel.plain(id: UI.company.id, lines: 1, weight: .regular, size: 13, color: .label)
+    let companyLbl = UILabel.plain(id: UI.company.id, lines: 1, alignment: .center, weight: .regular, size: 13, color: .label)
     let routeNoLbl = UILabel.plain(id: UI.routeNo.id, lines: 1, alignment: .center, weight: .semibold, size: 17, color: .label)
-    let originLbl  = UILabel.plain(id: UI.origin.id, lines: 1, weight: .regular, size: 17, color: .label)
-    let destinLbl  = UILabel.plain(id: UI.destin.id, lines: 1, weight: .regular, size: 17, color: .label)
+    let originLbl  = UILabel.plain(id: UI.origin.id, lines: 0, weight: .regular, size: 17, color: .label)
+    let destinLbl  = UILabel.plain(id: UI.destin.id, lines: 0, weight: .regular, size: 17, color: .label)
     let fromLbl    = UILabel.plain(id: UI.from.id, lines: 1, weight: .light, size: 13, color: .label)
     let toLbl      = UILabel.plain(id: UI.to.id, lines: 1, weight: .light, size: 13, color: .label)
     
-    let lblVstack  = UIStackView.vertical(spacing: 8, padding: .zero)
-    let lblHstack1 = UIStackView.horizontal(spacing: 4, padding: .zero, distribution: .fill)
-    let lblHstack2 = UIStackView.horizontal(spacing: 4, padding: .zero, distribution: .fill)
+    let leftLblVstack  = UIStackView.vertical(spacing: 8, padding: .zero)
+    let rightLblVstack  = UIStackView.vertical(spacing: 8, padding: .zero)
+    let lblHstack1 = UIStackView.horizontal(spacing: 6, padding: .zero, distribution: .fill)
+    let lblHstack2 = UIStackView.horizontal(spacing: 6, padding: .zero, distribution: .fill)
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -35,48 +36,42 @@ class RouteCellView: UITableViewCell {
         selectionStyle = .none
         backgroundColor = .clear
         
-        companyLbl.setContentHuggingPriority(.defaultLow + 2, for: .horizontal)
+        companyLbl.setContentHuggingPriority(.defaultLow + 1, for: .horizontal)
         routeNoLbl.setContentHuggingPriority(.defaultLow + 1, for: .horizontal)
-        toLbl.setContentHuggingPriority(.defaultLow + 1, for: .horizontal)
-        fromLbl.setContentHuggingPriority(.defaultLow + 1, for: .horizontal)
+        toLbl.setContentHuggingPriority(.defaultLow + 2, for: .horizontal)
+        fromLbl.setContentHuggingPriority(.defaultLow + 2, for: .horizontal)
         
         rootView.border(color: .separator)
     }
     
     func setLayout() {
         contentView.addSubview(rootView)
-        rootView.addSubview(companyLbl)
-        rootView.addSubview(routeNoLbl)
-        rootView.addSubview(lblVstack)
+        rootView.addSubview(leftLblVstack)
+        rootView.addSubview(rightLblVstack)
         
+        leftLblVstack.addSubviews([companyLbl, routeNoLbl])
         lblHstack1.addSubviews([fromLbl, originLbl])
         lblHstack2.addSubviews([toLbl, destinLbl])
-        lblVstack.addSubviews([lblHstack1, lblHstack2])
+        rightLblVstack.addSubviews([lblHstack1, lblHstack2])
         
         rootView.snp.makeConstraints {
             $0.top.left.right.equalToSuperview().inset(8)
             $0.bottom.equalToSuperview()
         }
         
-        companyLbl.snp.makeConstraints {
-            $0.left.equalToSuperview().inset(16)
-            $0.top.bottom.equalToSuperview().inset(12)
+        leftLblVstack.snp.makeConstraints {
+            $0.left.equalToSuperview().inset(12)
+            $0.centerY.equalTo(rightLblVstack.snp.centerY)
+            $0.width.equalToSuperview().multipliedBy(0.15)
         }
         
-        routeNoLbl.snp.makeConstraints {
-            $0.left.equalTo(companyLbl.snp.right).offset(16)
-            $0.top.bottom.equalToSuperview().inset(12)
-            $0.width.equalTo(48)
+        rightLblVstack.snp.makeConstraints {
+            $0.left.equalTo(leftLblVstack.snp.right).offset(18)
+            $0.top.bottom.right.equalToSuperview().inset(12)
         }
         
         toLbl.snp.makeConstraints {
             $0.width.equalTo(fromLbl.snp.width)
-        }
-        
-        lblVstack.snp.makeConstraints {
-            $0.left.equalTo(routeNoLbl.snp.right).offset(24)
-            $0.right.equalToSuperview().inset(8)
-            $0.top.bottom.equalToSuperview().inset(12)
         }
     }
     
